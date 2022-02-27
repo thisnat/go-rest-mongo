4b653de3b1a2c334038804c586521e7623e8003c
Nattawut <thisnattawut@gmail.com>
2022-02-27 21:08:13 +0700

    change bson

--- models/note.go
+++ models/note.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Note struct {
 	Id         primitive.ObjectID `bson:"_id" json:"id,omitempty"`
-	UserId     string             `json:"userId,omitempty" validate:"required"`
-	Content    string             `json:"content,omitempty" validate:"required"`
-	CreateDate primitive.DateTime `json:"createDate,omitempty"`
+	UserId     string             `bson:"userId" json:"userId,omitempty" validate:"required"`
+	Content    string             `bson:"content" json:"content,omitempty" validate:"required"`
+	CreateDate primitive.DateTime `bson:"createDate" json:"createDate,omitempty"`
 }
